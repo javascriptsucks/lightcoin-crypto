39e75c07537b6a3b45d50dabd7f58cc541d9760e
javascriptsucks <evix1988@live.cn>
2023-01-21 00:10:20 -0600

    oop-bank-system

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    return this.transactions.reduce((prev, curv) => prev + (curv instanceof Deposit ? curv.amount : -curv.amount), 0);
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
 
-  constructor(amount) {
-    this.amount = amount;
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    if (this instanceof Withdrawal && this.amount > this.account.balance) {
+      console.log('There is not enough balance for the withdrawal amount.');
+      return false;
+    }
+    this.account.addTransaction(this);
   }
 
 }
 
+class Deposit extends Transaction {
 
+  get value() {
+    return this.amount;
+  }
 
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(150.00, myAccount);
+t3.commit();
+
+console.log('Ending Balance:', myAccount.balance);
+console.log('Transaction:', myAccount.transactions);
+console.log('Ending Balance:', myAccount.balance);
